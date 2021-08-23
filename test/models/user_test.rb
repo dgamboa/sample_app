8b5208d7e8c0fccae49d0af00383abee8e2f4cc8
Daniel Gamboa <danielgamboa10@gmail.com>
2021-08-23 09:06:25 -0500

    Incorporate password validation and mechanics

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,8 +61,13 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "names should not be too long" do
-    @user.name = "a" * 51
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 end
